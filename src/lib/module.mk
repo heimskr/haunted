2ac9a7be4295ad92d52dca8ce0eea2a25f0af879
Kai Tamkun <ktamkun@ucsc.edu>
2019-05-30 03:27:26 -0700

    Added include/lib to include paths

--- src/lib/module.mk
+++ src/lib/module.mk
@@ -1,7 +1,7 @@
 UNISRC    := uniset uobject uvector uvectr32 uvectr64 unifilt unifunct unistr ustring cmemory bmpset unisetspan  \
              appendable stringpiece util patternprops uinvchar ustrtrns udataswp cstring umath uarrsort utf_impl \
              bytesinkutil charstr edits bytestream
-INCLUDE   += -Iicu/icu4c/source/common -Iicu/icu4c/source/common/unicode
+INCLUDE   += -Iinclude/lib
 COMMONSRC += $(patsubst %,src/lib/unicode/%.cpp,$(UNISRC))
 COMMONSRC += src/lib/utf8.cpp
 
