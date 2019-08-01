4a2d20c7b425f243d394e08b79ee8e13d6632625
Kai Tamkun <ktamkun@ucsc.edu>
2019-08-01 05:56:46 -0700

    Removed hbox & hbox_propo from /src/ui/boxes/module.mk.

--- src/ui/boxes/module.mk
+++ src/ui/boxes/module.mk
@@ -1,2 +1,1 @@
-COMMONSRC += src/ui/boxes/box.cpp src/ui/boxes/box_dual.cpp src/ui/boxes/box_propo.cpp src/ui/boxes/hbox.cpp
-COMMONSRC += src/ui/boxes/hbox_propo.cpp src/ui/boxes/simplebox.cpp
+COMMONSRC += src/ui/boxes/box.cpp src/ui/boxes/box_dual.cpp src/ui/boxes/box_propo.cpp src/ui/boxes/simplebox.cpp
