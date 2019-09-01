30acc598596d9551ce353bd55b2afde26cc0ef14
Kai Tamkun <ktamkun@ucsc.edu>
2019-08-31 23:23:52 -0700

    Hugely improved control::clear_rect()'s performance and chopped off 30 lines.

--- src/ui/control.cpp
+++ src/ui/control.cpp
@@ -73,43 +73,13 @@ namespace haunted::ui {
 	}
 
 	void control::clear_rect() {
-		if (term == nullptr)
-			return;
-
-		// There are four different ways to clear text on a row: erase the entire row, erase everything to the left of
-		// the cursor, erase everything to the right of the cursor and simply writing spaces. The first is ideal, the
-		// next two are okay and the last is the worst option. Which option we can use depends on the left-offset and
-		// width of the control.
-		term->out_stream.save();
-		if (pos.left == 0) {
-			// If we're at the left edge of the screen, we can clear-line if the width is full, or clear-left otherwise.
-			if (pos.width == term->get_cols()) {
-				for (int i = 0; i < pos.height; ++i) {
-					term->jump(0, pos.top + i);
-					term->clear_line();
-				}
-			} else {
-				for (int i = 0; i < pos.height; ++i) {
-					term->out_stream.jump(pos.width, pos.top + i);
-					term->out_stream.clear_left();
-				}
-			}
-		} else if (pos.left + pos.width == term->get_cols()) {
-			// If we're at the right edge of the screen, we can clear-right.
-			for (int i = 0; i < pos.height; ++i) {
-				term->out_stream.jump(pos.left, pos.top + i);
-				term->out_stream.clear_right();
-			}
-		} else {
-			// If the control doesn't reach either end of the screen, we have to print a bunch of spaces.
-			std::string spaces(pos.width, ' ');
-			for (int i = 0; i < pos.height; ++i) {
-				term->out_stream.jump(pos.left, pos.top + i);
-				std::cout << spaces;
-			}
+		if (term != nullptr) {
+			term->out_stream.save();
+			set_margins();
+			term->out_stream.clear();
+			reset_margins();
+			term->out_stream.restore();
 		}
-
-		term->out_stream.restore();
 	}
 
 	void control::flush() {
