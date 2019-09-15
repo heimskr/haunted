1eead3bb67f8868a969832268ef502f123a04b6e
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-14 21:22:48 -0700

    Fixed dummy_terminal letting jumps through

--- include/core/dummy_terminal.h
+++ include/core/dummy_terminal.h
@@ -12,37 +12,44 @@ namespace haunted {
 	 */
 	class dummy_terminal: public terminal {
 		private:
-			void apply() override;
-			void reset() override;
-			void winch(int, int) override;
-			void setattr(const termios &) override;
+			void apply() override {}
+			void reset() override {}
+			void winch(int, int) override {}
+			void setattr(const termios &) override {}
 
 		public:
 			dummy_terminal() {
 				suppress_output = true;
 			}
 
-			void cbreak() override;
-			void watch_size() override;
-			void redraw() override;
-			void set_root(ui::control *, bool) override;
-			void draw() override;
-			void start_input() override;
-			void flush() override;
-			void hmargins(size_t, size_t) override;
-			void hmargins() override;
-			void vmargins(size_t, size_t) override;
-			void vmargins() override;
-			void margins(size_t, size_t, size_t, size_t) override;
-			void margins() override;
-			void enable_hmargins() override;
-			void disable_hmargins() override;
-			void set_origin() override;
-			void reset_origin() override;
-			operator bool() const override;
-			terminal & operator>>(int &) override;
-			terminal & operator>>(char &) override;
-			terminal & operator>>(key &) override;
+			void cbreak() override {}
+			void watch_size() override {}
+			void redraw() override {}
+			void set_root(ui::control *, bool) override {}
+			void draw() override {}
+			void start_input() override {}
+			void flush() override {}
+			void hmargins(size_t, size_t) override {}
+			void hmargins() override {}
+			void vmargins(size_t, size_t) override {}
+			void vmargins() override {}
+			void margins(size_t, size_t, size_t, size_t) override {}
+			void margins() override {}
+			void enable_hmargins() override {}
+			void disable_hmargins() override {}
+			void set_origin() override {}
+			void reset_origin() override {}
+			void jump(int, int) override {}
+			void up(size_t) override {}
+			void down(size_t) override {}
+			void right(size_t) override {}
+			void left(size_t) override {}
+			void vscroll(int) override {}
+			void clear_line() override {}
+			operator bool() const override { return true; }
+			terminal & operator>>(int &)  override { return *this; }
+			terminal & operator>>(char &) override { return *this; }
+			terminal & operator>>(key &)  override { return *this; }
 	};
 }
 
--- src/core/module.mk
+++ src/core/module.mk
@@ -1,2 +1,1 @@
-COMMONSRC += src/core/hdefs.cpp           src/core/key.cpp  src/core/terminal.cpp  src/core/util.cpp  src/core/csi.cpp \
-             src/core/dummy_terminal.cpp
+COMMONSRC += src/core/hdefs.cpp  src/core/key.cpp  src/core/terminal.cpp  src/core/util.cpp  src/core/csi.cpp
