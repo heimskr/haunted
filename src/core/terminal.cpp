003f3791cb832abd681e31f1f5a18380f55e2438
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-16 00:10:28 -0700

    Removed some debug messages in terminal.

--- src/core/terminal.cpp
+++ src/core/terminal.cpp
@@ -21,7 +21,6 @@ namespace haunted {
 
 	terminal::terminal(std::istream &in_stream_, ansi::ansistream &out_stream_):
 	in_stream(in_stream_), out_stream(out_stream_), colors(&out_stream_, &output_mutex) {
-		DBG("terminal(): " << this);
 		original = attrs = getattr();
 		winsize size;
 		ioctl(STDIN_FILENO, TIOCGWINSZ, &size);
@@ -30,7 +29,6 @@ namespace haunted {
 	}
 
 	terminal::~terminal() {
-		DBG("~terminal(): " << this);
 		out_stream.reset_colors();
 		out_stream.clear();
 		reset();
@@ -230,10 +228,8 @@ namespace haunted {
 	}
 
 	void terminal::join() {
-		if (input_thread.joinable()) {
-			DBG("Joining input thread.");
+		if (input_thread.joinable())
 			input_thread.join();
-		}
 	}
 
 	void terminal::flush() {
